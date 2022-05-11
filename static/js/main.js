0102959b87d0ebc9d0ae6d2df0d9cdf833ed9c13
LucaDeCicco <luca14.decicco@yahoo.com>
2022-05-11 11:26:56 +0300

    open modal but we can close

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,16 @@
 import {boardsManager} from "./controller/boardsManager.js";
 import {domManager} from "./view/domManager.js";
 import {dataHandler} from "./data/dataHandler.js";
+import {htmlFactory, htmlTemplates} from "./view/htmlFactory.js";
 
 function init() {
     boardsManager.loadBoards();
     const createBoardBtn = document.querySelector('#createboard')
-    createBoardBtn.addEventListener('click', () => {dataHandler.createNewBoard('ceva');document.querySelector('#root').innerHTML="" ; boardsManager.loadBoards()})
+    createBoardBtn.addEventListener('click', () => {
+        domManager.addChild('#root', htmlFactory(htmlTemplates.modal)())
+    })
+
+    // createBoardBtn.addEventListener('click', () => {dataHandler.createNewBoard('ceva');document.querySelector('#root').innerHTML="" ; boardsManager.loadBoards()})
 
 }
 
--- static/js/view/htmlFactory.js
+++ static/js/view/htmlFactory.js
@@ -1,13 +1,10 @@
 export const htmlTemplates = {
-    board: 1,
-    card: 2,
-    statuses: 3
+    board: 1, card: 2, statuses: 3, modal: 4
 }
 
 export const builderFunctions = {
-    [htmlTemplates.board]: boardBuilder,
-    [htmlTemplates.statuses]: statusesBuilder,
-    [htmlTemplates.card]: cardBuilder
+    [htmlTemplates.board]: boardBuilder, [htmlTemplates.statuses]: statusesBuilder, [htmlTemplates.card]: cardBuilder,
+    [htmlTemplates.modal]: createModal,
 };
 
 export function htmlFactory(template) {
@@ -63,3 +60,22 @@ function cardBuilder(card) {
     return `<div class="card" data-card-id="${card.id}">${card.title}</div>`;
 }
 
+function createModal() {
+    return `<div class="modal fade show" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="false" style="display: block">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>`
+}
