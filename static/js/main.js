641a9bef283c0bce2f9e134c8c6fc3e2e89cea93
LucaDeCicco <luca14.decicco@yahoo.com>
2022-05-12 11:08:10 +0300

    bug if you add a new board you can t edit board title

--- static/js/main.js
+++ static/js/main.js
@@ -15,7 +15,6 @@ async function init() {
             modalElement.remove()
         });
         addNewBoard();
-        changeBoardTitle();
 
     })
     changeBoardTitle();
@@ -32,27 +31,35 @@ function addNewBoard() {
         modalElement.remove()
         document.querySelector('#root').innerHTML = "";
         boardsManager.loadBoards()
+        changeBoardTitle();
+
     })
 
 }
 
 // let isBtn = true
 
+
+function changeBoardTitleHandler(boardTitle) {
+    let isBtn = Array.from(boardTitle.closest(".board-content").querySelectorAll('.saveBtn'))
+    console.log(isBtn)
+    if (isBtn.length < 1) {
+        domManager.addChild(`.putHereSaveBtn[data-board-id="${boardTitle.dataset.boardId}"]`, `<button class="saveBtn">Save</button>`)
+        // isBtn = false
+        saveNewTitle(boardTitle.dataset.boardId)
+    }
+}
+
+
 function changeBoardTitle() {
+    console.log("intra")
     const boardTitles = document.querySelectorAll(`span.boardtitle[data-board-id]`)
 
     for (let boardTitle of boardTitles) {
-        boardTitle.addEventListener('click', (ev) => {
-
-            let isBtn = Array.from(boardTitle.closest(".board-content").querySelectorAll('.saveBtn'))
-            console.log(isBtn)
-            if (isBtn.length < 1) {
-                domManager.addChild(`.putHereSaveBtn[data-board-id="${ev.target.dataset.boardId}"]`, `<button class="saveBtn">Save</button>`)
-                // isBtn = false
-                saveNewTitle(ev.target.dataset.boardId)
-            }
-        },)
+        boardTitle.removeEventListener('click', () => changeBoardTitleHandler(boardTitle),)
+        boardTitle.addEventListener('click', () => changeBoardTitleHandler(boardTitle),)
     }
+
 }
 
 
