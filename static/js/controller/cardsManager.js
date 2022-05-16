0bb7804a4b1d5ff699e0046fb530b1ce60c2b01f
LucaDeCicco <luca14.decicco@yahoo.com>
2022-05-16 13:40:54 +0300

    semi good

--- static/js/controller/cardsManager.js
+++ static/js/controller/cardsManager.js
@@ -1,7 +1,7 @@
 import {dataHandler} from "../data/dataHandler.js";
 import {htmlFactory, htmlTemplates} from "../view/htmlFactory.js";
 import {domManager} from "../view/domManager.js";
-import {boardsManager} from "./boardsManager";
+import {boardsManager} from "./boardsManager.js";
 
 export let cardsManager = {
     loadCards: async function (boardId) {
